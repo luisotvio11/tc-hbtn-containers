b9528fa38f512d9932678840e6533e2bca39b73b
luisotv <luisotavio@ciandt.com>
2022-05-03 15:26:04 -0300

    task 0

---
+++ 0x03/MessageResource.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class MessageResource {
+	
+	 @GetMapping("/simpleMessageWelcome")
+	   public String simpleMessageWelcome() {
+	      var hostName = System.getenv("HOSTNAME");
+	      return "environment: " + hostName;
+	   }
+
+}
